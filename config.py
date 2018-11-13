e7ac16348fd64bc4ac3cecc336de79417f298c06
tclerico <timclerico@gmail.com>
2018-11-13 14:18:29 -0500

    more updates for deploy

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ def readin(file):
     return clean
 
 class Config(object):
-    key = readin('key.txt')
-    emailInfo = readin('emailIn.txt')
-    salt = readin('passwdSalt.txt')
-    SECRET_KEY = os.environ.get('SECRET_KEY') or key[0]
-    SECURITY_PASSWORD_SALT = salt[0]
+    # key = os.environ.ge
+    # emailInfo = readin('emailIn.txt')
+    # salt = readin('passwdSalt.txt')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECURITY_PASSWORD_SALT = os.environ.get('PASSWORD_SALT')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -22,8 +22,8 @@ class Config(object):
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_USERNAME = emailInfo[0]
-    MAIL_PASSWORD = emailInfo[1]
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['saacithaca@gmail.com']
     LOG_TO_STDOU = os.environ.get('LOG_TO_STDOUT')
 
