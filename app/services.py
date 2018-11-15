e7b53d59ac186a1be4742c1acb326f995b4806cb
tclerico <timclerico@gmail.com>
2018-11-15 17:43:45 -0500

    sql alchemy issue resolving

--- app/services.py
+++ app/services.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 
 
 def subjects_from_file():
-    with open('subjects.txt', encoding='utf-8') as f:
+    with open('subjects.txt') as f:
         subjects = f.readlines()
 
     # strip all new line characters
